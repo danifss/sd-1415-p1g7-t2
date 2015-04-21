f4c5ed1580518f7637b2f60691ea86755acd4072
daniel.silva <daniel.silva@ua.pt>
2015-04-21 16:42:37 +0000

    INITIAL COMMIT

---
+++ T2_TP1_G7/src/Customer/CustomerInterface.java
@@ -0,0 +1,15 @@
+package Customer;
+
+/**
+ * @author Daniel 51908
+ * @author Raphael 64044
+ * @version 1.0
+ */
+public interface CustomerInterface {
+
+    /**
+     * Life cycle of the customer.
+     */
+    void run();
+    
+}
