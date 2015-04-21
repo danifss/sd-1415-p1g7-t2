26c75816cc81d714e208ac8d1d882d8043c4fca4
daniel.silva@ua.pt <daniel.silva@ua.pt@84c44828-5e89-4879-bf44-8a2479a86aee>
2015-04-21 22:53:07 +0000

    Criado o Broker do Customer. Criada interface para o broker do customer usar.

---
+++ T2_TP1_G7/src/Customer/CustomerBroker.java
@@ -0,0 +1,40 @@
+package Customer;
+
+/**
+ * @author Daniel 51908
+ * @author Raphael 64044
+ * @version 2.0
+ */
+public class CustomerBroker implements ShopInterface {
+
+    @Override
+    public boolean endOper() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void enterShop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void exitShop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void iWantThis(int customerId, int nGoods) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isDoorOpen() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int perusingAround() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
