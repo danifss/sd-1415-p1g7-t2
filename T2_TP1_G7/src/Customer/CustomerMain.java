1701c939794ccda9e9bc0f0db30caa627d84cda1
daniel.silva <daniel.silva@ua.pt>
2015-04-27 08:52:45 +0000

    CustomerMain: Feito o main do Customer, não definitivo.

--- T2_TP1_G7/src/Customer/CustomerMain.java
+++ T2_TP1_G7/src/Customer/CustomerMain.java
@@ -7,5 +7,16 @@ package Customer;
 public class CustomerMain {
     public static void main(String[] args){
         //TODO: create and start 3 Customers
+        int nCustomers = 3;
+        Customer[] customer = new Customer[nCustomers];
+        CustomerBroker broker = new CustomerBroker("0.0.0.0", 220101, "0.0.0.0", 220102);
+        
+        //Initialization of Customers
+        for (int i = 0; i < nCustomers; i++)
+            customer[i] = new Customer(broker, i, broker);
+        
+        // Starting Customers
+        for (int i = 0; i < nCustomers; i++)
+            customer[i].start();
     }
 }
