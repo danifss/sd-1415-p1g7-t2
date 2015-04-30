f477632349df8ea417b8e0e98097fda5a9052c97
daniel.silva <daniel.silva@ua.pt>
2015-04-30 00:12:02 +0000

    Craftman: Acabado a parte cliente.

--- P2_TP1_G7/src/Craftman/CraftmanBroker.java
+++ P2_TP1_G7/src/Craftman/CraftmanBroker.java
@@ -204,44 +204,229 @@ public class CraftmanBroker implements CraftmanRepositoryInterface, CraftmanShop
         return result;
     }
 
+    /**
+     * Communicate with Factory: The Craftman checks if the Factory has prime materials to collect.
+     * @param craftmanId
+     * @return if Factory has prime materials
+     */
     @Override
     public boolean checkForMaterials(int craftmanId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ClientCom con = new ClientCom(FactoryServerHostName, FactoryServerPortNumb);
+        MessageFactory inMessage, outMessage;
+        
+        outMessage = new MessageFactory(MessageFactory.CHECKFORMATERIALS, craftmanId);
+        con.writeObject(outMessage);
+        inMessage = (MessageFactory) con.readObject();
+        boolean result = false;
+        switch(inMessage.getType()){
+            case MessageFactory.ACK:
+                result = inMessage.isBool();
+                break;
+            default:
+                GenericIO.writelnString("Craftman: "+craftmanId+" - Error checking for prime materials on Factory.");
+                GenericIO.writelnString(inMessage.toString());
+                System.exit(1);
+                break;
+        }
+        con.close();
+        return result;
     }
 
+    /**
+     * Communicate with Factory: Check if the Craftman needs to contact owner to bring prime materials.
+     * @param craftmanId
+     * @return if owner has to bring prime materials from Factory
+     */
     @Override
     public boolean checkForRestock(int craftmanId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ClientCom con = new ClientCom(FactoryServerHostName, FactoryServerPortNumb);
+        MessageFactory inMessage, outMessage;
+        
+        outMessage = new MessageFactory(MessageFactory.CHECKFORRESTOCK, craftmanId);
+        con.writeObject(outMessage);
+        inMessage = (MessageFactory) con.readObject();
+        boolean result = false;
+        switch(inMessage.getType()){
+            case MessageFactory.ACK:
+                result = inMessage.isBool();
+                break;
+            default:
+                GenericIO.writelnString("Craftman: "+craftmanId+" - Error checking if needs to contact owner to bring prime materials from Factory.");
+                GenericIO.writelnString(inMessage.toString());
+                System.exit(1);
+                break;
+        }
+        con.close();
+        return result;
     }
 
+    /**
+     * Communicate with Factory: The Craftman collects prime materials.
+     * @param craftmanId
+     * @return 
+     */
     @Override
     public int collectMaterials(int craftmanId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ClientCom con = new ClientCom(FactoryServerHostName, FactoryServerPortNumb);
+        MessageFactory inMessage, outMessage;
+        
+        outMessage = new MessageFactory(MessageFactory.COLLECTMATERIALS, craftmanId);
+        con.writeObject(outMessage);
+        inMessage = (MessageFactory) con.readObject();
+        int result = 0;
+        switch(inMessage.getType()){
+            case MessageFactory.ACK:
+                result = inMessage.getValue();
+                break;
+            default:
+                GenericIO.writelnString("Craftman: "+craftmanId+" - Error checking if needs to contact owner to bring prime materials from Factory.");
+                GenericIO.writelnString(inMessage.toString());
+                System.exit(1);
+                break;
+        }
+        con.close();
+        return result;
     }
 
+    /**
+     * Communicate with Factory: Checks if the all the prime materials from the storage were supplied.
+     * @param craftmanId
+     * @return if all prime materials have been supplied
+     */
     @Override
     public boolean endOfPrimeMaterials(int craftmanId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ClientCom con = new ClientCom(FactoryServerHostName, FactoryServerPortNumb);
+        MessageFactory inMessage, outMessage;
+        
+        outMessage = new MessageFactory(MessageFactory.ENDOFPRIMEMATERIALS, craftmanId);
+        con.writeObject(outMessage);
+        inMessage = (MessageFactory) con.readObject();
+        boolean result = false;
+        switch(inMessage.getType()){
+            case MessageFactory.ACK:
+                result = inMessage.isBool();
+                break;
+            default:
+                GenericIO.writelnString("Craftman: "+craftmanId+" - Error checking if needs to contact owner to bring prime materials from Factory.");
+                GenericIO.writelnString(inMessage.toString());
+                System.exit(1);
+                break;
+        }
+        con.close();
+        return result;
     }
 
+    /**
+     * Communicate with Factory: The Craftman sees if someone already contacted the owner to restock prime materials.
+     * @param craftmanId
+     * @return if someone already contacted the owner
+     */
     @Override
     public boolean flagPrimeActivated(int craftmanId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ClientCom con = new ClientCom(FactoryServerHostName, FactoryServerPortNumb);
+        MessageFactory inMessage, outMessage;
+        
+        outMessage = new MessageFactory(MessageFactory.FLAGPRIMEACTIVATED, craftmanId);
+        con.writeObject(outMessage);
+        inMessage = (MessageFactory) con.readObject();
+        boolean result = false;
+        switch(inMessage.getType()){
+            case MessageFactory.ACK:
+                result = inMessage.isBool();
+                break;
+            default:
+                GenericIO.writelnString("Craftman: "+craftmanId+" - Error checking if someone already contacted the owner to restock on Factory.");
+                GenericIO.writelnString(inMessage.toString());
+                System.exit(1);
+                break;
+        }
+        con.close();
+        return result;
     }
 
+    /**
+     * Communicate with Factory: Craftman sees how many prime materials needs to produce a new product
+     * @param craftmanId
+     * @return number of prime materials needed per product
+     */
     @Override
     public int getnPrimePerProduct(int craftmanId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ClientCom con = new ClientCom(FactoryServerHostName, FactoryServerPortNumb);
+        MessageFactory inMessage, outMessage;
+        
+        outMessage = new MessageFactory(MessageFactory.GETNPRIMEPERPRODUCT, craftmanId);
+        con.writeObject(outMessage);
+        inMessage = (MessageFactory) con.readObject();
+        int result = 0;
+        switch(inMessage.getType()){
+            case MessageFactory.ACK:
+                result = inMessage.getValue();
+                break;
+            default:
+                GenericIO.writelnString("Craftman: "+craftmanId+" - Error checking how many prime materials need to make a product on Factory.");
+                GenericIO.writelnString(inMessage.toString());
+                System.exit(1);
+                break;
+        }
+        con.close();
+        return result;
     }
 
+    /**
+     * Communicate with Factory: The Craftman stores the products produced.
+     * @param craftmanId
+     * @param nProd Number of products that the Craftman has
+     * @return number of products the Craftman stored
+     */
     @Override
     public int goToStore(int craftmanId, int nProd) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ClientCom con = new ClientCom(FactoryServerHostName, FactoryServerPortNumb);
+        MessageFactory inMessage, outMessage;
+        
+        outMessage = new MessageFactory(MessageFactory.GOTOSTORE, craftmanId, nProd);
+        con.writeObject(outMessage);
+        inMessage = (MessageFactory) con.readObject();
+        int result = 0;
+        switch(inMessage.getType()){
+            case MessageFactory.ACK:
+                result = inMessage.getValue();
+                break;
+            default:
+                GenericIO.writelnString("Craftman: "+craftmanId+" - Error storing made products into Factory.");
+                GenericIO.writelnString(inMessage.toString());
+                System.exit(1);
+                break;
+        }
+        con.close();
+        return result;
     }
 
+    /**
+     * Communicate with Factory: The Craftman turns true the flag that indicates that prime materials are needed.
+     * @param craftmanId
+     * @return if he contacted the owner
+     */
     @Override
     public boolean primeMaterialsNeededFactory(int craftmanId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ClientCom con = new ClientCom(FactoryServerHostName, FactoryServerPortNumb);
+        MessageFactory inMessage, outMessage;
+        
+        outMessage = new MessageFactory(MessageFactory.PRIMEMATERIALSNEEDED, craftmanId);
+        con.writeObject(outMessage);
+        inMessage = (MessageFactory) con.readObject();
+        boolean result = false;
+        switch(inMessage.getType()){
+            case MessageFactory.ACK:
+                result = inMessage.isBool();
+                break;
+            default:
+                GenericIO.writelnString("Craftman: "+craftmanId+" - Error checking if flag of prime materials needed is true or not in Factory.");
+                GenericIO.writelnString(inMessage.toString());
+                System.exit(1);
+                break;
+        }
+        con.close();
+        return result;
     }
     
 }
--- P2_TP1_G7/src/comInf/MessageFactory.java
+++ P2_TP1_G7/src/comInf/MessageFactory.java
@@ -26,8 +26,62 @@ public class MessageFactory extends Message {
      */
     public static final int CHECKCONTACTPRODUCT = 2;
     
+    /**
+     * Message type: The Craftman checks if the Factory has prime materials to collect.
+     * @serialField CHECKFORMATERIALS
+     */
+    public static final int CHECKFORMATERIALS = 3;
+    
+    /**
+     * Message type: Check if the Craftman needs to contact owner to bring prime materials.
+     * @serialField CHECKFORRESTOCK
+     */
+    public static final int CHECKFORRESTOCK = 4;
+    
+    /**
+     * Message type: The Craftman collects prime materials.
+     * @serialField COLLECTMATERIALS
+     */
+    public static final int COLLECTMATERIALS = 5;
+    
+    /**
+     * Message type: Checks if the all the prime materials from the storage were supplied.
+     * @serialField ENDOFPRIMEMATERIALS
+     */
+    public static final int ENDOFPRIMEMATERIALS = 6;
+    
+    /**
+     * Message type: The Craftman sees if someone already contacted the owner to restock prime materials.
+     * @serialField FLAGPRIMEACTIVATED
+     */
+    public static final int FLAGPRIMEACTIVATED = 7;
+    
+    /**
+     * Message type: Craftman sees how many prime materials needs to produce a new product
+     * @serialField GETNPRIMEPERPRODUCT
+     */
+    public static final int GETNPRIMEPERPRODUCT = 8;
+    
+    /**
+     * Message type: The Craftman stores the products produced.
+     * @serialField GOTOSTORE
+     */
+    public static final int GOTOSTORE = 9;
+    
+    /**
+     * Message type: The Craftman turns true the flag that indicates that prime materials are needed.
+     * @serialField PRIMEMATERIALSNEEDED
+     */
+    public static final int PRIMEMATERIALSNEEDED = 10;
+    
     
     //*************** Internal Variables
+    /**
+     * Variable to save a integer value.
+     * @serialField value
+     */
+    private int value = 0;
+    
     /**
      * Generic boolean variable
      * @serialField bool
@@ -54,7 +108,18 @@ public class MessageFactory extends Message {
     }
     
     /**
-     * Message Factory Constructor (form 3)
+     * Message Factory constructor (form 3)
+     * @param type Message type
+     * @param id ID
+     * @param value Variable value
+     */
+    public MessageFactory(int type, int id, int value) {
+        super(type, id);
+        this.value = value;
+    }
+    
+    /**
+     * Message Factory Constructor (form 4)
      * @param type Message type
      * @param id
      * @param bool Generic boolean variable
@@ -65,6 +130,13 @@ public class MessageFactory extends Message {
     }
 
     //*************** Internal Functions
+    /**
+     * Get a generic integer value.
+     * @return value variable
+     */
+    public int getValue() {
+        return value;
+    }
 
     /**
      * Get generic boolean variable.
