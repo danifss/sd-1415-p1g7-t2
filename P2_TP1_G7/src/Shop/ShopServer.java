f9ade671a98b7a1918c9f6f729887a7ec425f8fe
daniel.silva@ua.pt <daniel.silva@ua.pt@84c44828-5e89-4879-bf44-8a2479a86aee>
2015-05-02 18:03:33 +0000

    Criado um broker do Shop para o Repository. Instanciação do repositorio no ShopServer. Parte servidor do Storage terminado.

--- P2_TP1_G7/src/Shop/ShopServer.java
+++ P2_TP1_G7/src/Shop/ShopServer.java
@@ -12,16 +12,18 @@ public class ShopServer {
      * Server Shop port number.
      * @serialField portNumb
      */
-    private static final int portNumb = 22170;
+    private static final int portNumb = 22171;
     
     public static void main(String[] args){
-        //TODO: Aqui e onde corre o servidor e lanca threads(ClientProxy) para atender cada cliente.
+        // Aqui e onde corre o servidor e lanca threads(ClientProxy) para atender cada cliente.
         Shop shop;                                              // Shop (servico a ser prestado)
         ShopBroker shopInterface;                               // Interface a Shop
         ServerCom scon, sconi;                                  // canais de comunicacao
         ClientProxy cliProxy;                                   // thread agente prestador do servico
+        ShopBrokerRepository repository;                        // Repository Broker client
 
         int nCustomers = 3;
+        int nCraftmans = 3;
         int nInitialProductsInShop = 10;                        // Initial number of products in the Shop
         int nInitialPrimeMaterialsInStorage = 20;               // Initial number of prime materials in the Storage
         int nPrimeMaterialsInFactory = 10;                      // Initial number of prime materials in the Factory
@@ -31,8 +33,10 @@ public class ShopServer {
         /* estabelecimento do servico */
         scon = new ServerCom(portNumb);                         // criacao do canal de escuta e sua associacao
         scon.start();                                           // com o endereco publico
+        
+        repository = new ShopBrokerRepository("localhost", 22170); // ativacao do servico
         shop = new Shop(nInitialProductsInShop, nCustomers, repository, totalProducts); // activacao do servico
-        shopInterface = new ShopBroker(shop, nCustomers);       // activacao do interface com o servico
+        shopInterface = new ShopBroker(shop, nCustomers, nCraftmans); // activacao do interface com o servico
         GenericIO.writelnString("O serviço foi estabelecido!");
         GenericIO.writelnString("O servidor esta em escuta.");
 
