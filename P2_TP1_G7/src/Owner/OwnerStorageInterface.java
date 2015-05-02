8026565d786a30b665f53f0c3345ee228e1de34e
raphael.carvalho <raphael.carvalho@ua.pt>
2015-05-02 03:51:08 +0000

    PackageOwner: Correção de javadoc e removido import não utilizado

--- P2_TP1_G7/src/Owner/OwnerStorageInterface.java
+++ P2_TP1_G7/src/Owner/OwnerStorageInterface.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Owner;
 
 /**
  * @author Daniel 51908
  * @author Raphael 64044
- * @version 1.0
+ * @version 2.0
  */
 
 public interface OwnerStorageInterface {
