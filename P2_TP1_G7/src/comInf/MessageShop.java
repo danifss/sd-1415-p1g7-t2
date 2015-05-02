0f231b2178ed844d3b7123f5a2486e4c8763f203
raphael.carvalho@ua.pt <raphael.carvalho@ua.pt@84c44828-5e89-4879-bf44-8a2479a86aee>
2015-05-02 01:17:17 +0000

    MessageShop: Adicionado tipos de mensagens relativas ao Owner

--- P2_TP1_G7/src/comInf/MessageShop.java
+++ P2_TP1_G7/src/comInf/MessageShop.java
@@ -76,6 +76,92 @@ public class MessageShop extends Message {
     public static final int READYFORTRANSFER = 10;
     
     
+    //*************** OWNER MESSAGE TYPES
+    /**
+     * Message type: Owner opens the shop.
+     * @serialField OPENTHESHOP
+     */
+    public static final int OPENTHESHOP = 30;
+    
+    /**
+     * Message type: Owner checks if the Factory needs prime materials.
+     * @serialField ISSUPPLYMATERIALSTOFACT
+     */
+    public static final int ISSUPPLYMATERIALSTOFACT = 31;
+    
+    /**
+     * Message type: Owner checks if the he can collect products in the Factory.
+     * @serialField ISTRANFSPRODSTOSHOP
+     */
+    public static final int ISTRANFSPRODSTOSHOP = 32;
+    
+    /**
+     * Message type: Owner sees the situation of the shop and decide what to do.
+     * @serialField APPRAISESIT
+     */
+    public static final int APPRAISESIT = 33;
+    
+    /**
+     * Message type: Owner closes the door.
+     * @serialField CLOSETHEDOOR
+     */
+    public static final int CLOSETHEDOOR = 34;
+    
+    /**
+     * Message type: Owner sees if there is customers inside the shop.
+     * @serialField CUSTOMERSINTHESHOP
+     */
+    public static final int CUSTOMERSINTHESHOP = 35;
+    
+    /**
+     * Message type: Owner address a Customer on the queue.
+     * @serialField ADDRESSACUSTOMER
+     */
+    public static final int ADDRESSACUSTOMER = 36;
+    
+    /**
+     * Message type: Owner services a customer.
+     * @serialField SERVICECUSTOMER
+     */
+    public static final int SERVICECUSTOMER = 37;
+    
+    /**
+     * Message type: Owner says goodbye to the Customer he is attending.
+     * @serialField SAYGOODBYETOCUST
+     */
+    public static final int SAYGOODBYETOCUST = 38;
+    
+    /**
+     * Message type: Owner sees if the shop is on state STILL_OPEN.
+     * @serialField ISSHOPSTILLOPEN
+     */
+    public static final int ISSHOPSTILLOPEN = 39;
+    
+    /**
+     * Message type: Owner goes to the Factory to collect products.
+     * @serialField GOTOWORKSHOP
+     */
+    public static final int GOTOWORKSHOP = 40;
+    
+    /**
+     * Message type: Owner updates the number of products that the shop is selling.
+     * @serialField ADDNGOODSINDISPLAY
+     */
+    public static final int ADDNGOODSINDISPLAY = 41;
+    
+    /**
+     * Message type: Owner goes to Factory to restock prime materials.
+     * @serialField REPLENISHSTOCK
+     */
+    public static final int REPLENISHSTOCK = 42;
+    
+    /**
+     * Message type: Verifies if the Owner can stop working.
+     * @serialField ENDOPEROWNER
+     */
+    public static final int ENDOPEROWNER = 43;
+    
+    
     //*************** Internal variables
     /**
      * Variable to save a integer value.
