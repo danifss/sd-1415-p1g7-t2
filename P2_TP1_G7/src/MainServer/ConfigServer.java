644a3c99736cfe2b01bb278d481815f9ef03bfcc
daniel.silva@ua.pt <daniel.silva@ua.pt@84c44828-5e89-4879-bf44-8a2479a86aee>
2015-05-03 15:46:34 +0000

    Criada a maquina de configuração estática.

---
+++ P2_TP1_G7/src/MainServer/ConfigServer.java
@@ -0,0 +1,19 @@
+package MainServer;
+
+/**
+ *
+ * @author silva
+ */
+public class ConfigServer {
+    
+    /**
+     * Configurations Server port number.
+     * 
+     * @serialField portNumb
+     */
+    private static final int portNumb = 22177;
+    
+    public static void main(String[] args){
+        
+    }
+}
