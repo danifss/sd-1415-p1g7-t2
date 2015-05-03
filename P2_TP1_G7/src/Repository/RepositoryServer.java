39167c3e93923c97331c7ad4f66a325094ef48e9
daniel.silva <daniel.silva@ua.pt>
2015-05-03 15:46:34 +0000

    Criada a maquina de configuração estática.

--- P2_TP1_G7/src/Repository/RepositoryServer.java
+++ P2_TP1_G7/src/Repository/RepositoryServer.java
@@ -1,18 +1,20 @@
 package Repository;
 
 import genclass.GenericIO;
+
 /**
  *
  * @author Daniel
  */
 public class RepositoryServer {
-    
+
     /**
      * Server Repository port number.
+     *
      * @serialField portNumb
      */
     private static final int portNumb = 22170;
-    
+
     public static void main(String[] args){
         //TODO: Aqui e onde corre o servidor e lanca threads(ClientProxy) para atender cada cliente.
         Repository repository;                                  // Repository (servico a ser prestado)
@@ -24,7 +26,7 @@ public class RepositoryServer {
         int nCraftmans = 3;
         String fName = "log.txt";
         int nPrimeMaterialsInFactory = 10;                      // Initial number of prime materials in the Factory
-        
+
         /* estabelecimento do servico */
         scon = new ServerCom(portNumb);                         // criacao do canal de escuta e sua associacao
         scon.start();                                           // com o endereco publico
@@ -35,7 +37,7 @@ public class RepositoryServer {
         GenericIO.writelnString("O servidor esta em escuta.");
 
         /* processamento de pedidos */
-        while (true) {
+        while (true){
             sconi = scon.accept();                              // entrada em processo de escuta
             cliProxy = new ClientProxy(sconi, repositoryInterface);    // lancamento do agente prestador do servico
             cliProxy.start();
