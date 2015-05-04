f8b3071144f1abc9a545f63876a9ac911dc31c3d
raphael.carvalho <raphael.carvalho@ua.pt>
2015-05-04 02:27:18 +0000

    Atualizado package-info

--- P2_TP1_G7/src/Factory/package-info.java
+++ P2_TP1_G7/src/Factory/package-info.java
@@ -1,6 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Package were is stored the monitor Factory were the Craftman will work.
  */
 package Factory;
